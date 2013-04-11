0d4f8c58c04ecb5f413264c0e640c5929d44d480
Haney Maxwell <hmrmaxwell@gmail.com>
2013-04-11 09:26:08 -0500

    the message needs to be this simple, so I think it's ok if it's not opaque

--- actor.h
+++ actor.h
@@ -2,8 +2,12 @@
 #define SIMPLEACTOR_ACTOR_H
 
 typedef struct actor actor;
-typedef struct message message;
 typedef struct message_handler message_handler;
+
+typedef struct {
+    int msgtype;
+    void * data;
+} message;
 
 actor * create_actor(message_handler ** handler); //consumes the message handler
 int send_message_to(actor * recipient, message msg);
